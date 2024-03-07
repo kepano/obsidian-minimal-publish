c2db3a54757dac1f4aa92131db9404f0e4646eaa
Steph Ango <10565871+kepano@users.noreply.github.com>
2024-03-07 12:35:33 -0800

    Remove plausible code

--- publish.js
+++ publish.js
@@ -1,9 +1,3 @@
-var plausible = document.createElement('script')
-plausible.defer = true
-plausible.setAttribute('data-domain', 'minimal.guide')
-plausible.src = 'https://plausible.io/js/script.outbound-links.js'
-document.head.appendChild(plausible)
-
 publish.registerMarkdownPostProcessor(async (el, ctx) => {
   const images = el.querySelectorAll('.internal-embed');
   images.forEach(image => {
