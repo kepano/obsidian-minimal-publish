327c28a8cabb131841b8328237415871d52f5614
Steph Ango <10565871+kepano@users.noreply.github.com>
2024-03-07 10:48:30 -0800

    Add escape listener

--- publish.js
+++ publish.js
@@ -1,46 +1,34 @@
 var plausible = document.createElement('script')
 plausible.defer = true
 plausible.setAttribute('data-domain', 'minimal.guide')
-plausible.src = 'https://plausible.io/js/plausible.js'
+plausible.src = 'https://plausible.io/js/script.outbound-links.js'
 document.head.appendChild(plausible)
 
+publish.registerMarkdownPostProcessor(async (el, ctx) => {
+  const images = el.querySelectorAll('.internal-embed');
+  images.forEach(image => {
+    image.addEventListener('click', function() {
 
-var siteLeft = document.querySelector('.site-body-left-column');
+      const lightboxDiv = document.createElement('div');
+      lightboxDiv.classList.add('lightbox');
 
-let navOrderAsc = ["Home.md", "Help.md"]; /* these go on top*/
+      const contentToMove = this.cloneNode(true);
+      lightboxDiv.appendChild(contentToMove);
 
-let navOrderDsc = []; /* these go at the bottom */
+      document.body.appendChild(lightboxDiv);
 
-/* items not mentioned go in between in alphabetical order */
+      const removeLightbox = () => {
+        document.body.removeChild(lightboxDiv);
+        document.removeEventListener('keydown', escapeKeyListener);
+      };
 
-var siteNav = siteLeft.querySelector('.nav-view-outer');
-
-var navContainer = siteNav.querySelector('.tree-item').querySelector('.tree-item-children');
-
-for (const item of navOrderAsc.reverse()){
-
-    querytext = '[data-path="' + item + '"]';
-
-    navItem = navContainer.querySelector(querytext);
-
-    if (navItem == null) continue;
-
-    moveItem = navItem.parentElement;
-
-    navContainer.prepend(moveItem);
-
-}
-
-for (const item of navOrderDsc.reverse()){
-
-    querytext = '[data-path="' + item + '"]';
-
-    navItem = navContainer.querySelector(querytext);
-
-    if (navItem == null) continue;
-
-    moveItem = navItem.parentElement;
-
-    navContainer.append(moveItem);
-
-}
+      lightboxDiv.addEventListener('click', removeLightbox);
+      const escapeKeyListener = (event) => {
+        if (event.key === "Escape") {
+          removeLightbox();
+        }
+      };
+      document.addEventListener('keydown', escapeKeyListener);
+    });
+  });
+});
